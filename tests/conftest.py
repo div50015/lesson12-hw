adffb9849b6914b2810291070a9474a9ba968621
Igor Degtyarenko <div50015@mail.ru>
2023-10-27 15:49:11 +0300

    editing conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,6 @@ from selene import browser
 from utils import attach
 
 
-
-
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     options = Options()
@@ -24,17 +22,15 @@ def browser_management():
         options=options
     )
 
-
-
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.timeout = 2.0
     browser.config.window_width = 1500
     browser.config.window_height = 1080
-
     # закрытие сообщения о том что браузер запущен в отладочном режиме
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    browser.config.driver_options = driver_options
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    # browser.config.driver_options = driver_options
 
     yield browser
 
