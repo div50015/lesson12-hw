158323a3cf5857c9980b9b09be7cb42f8b177da7
Igor Degtyarenko <div50015@mail.ru>
2023-11-02 10:41:00 +0300

    Add donenv & user-password

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 from utils import attach
+import os
+from dotenv import load_dotenv
+
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+@pytest.fixture(scope='session')
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def browser_management(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -17,20 +35,15 @@ def browser_management():
         }
     }
     options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub", options=options)
 
     browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.timeout = 2.0
     browser.config.window_width = 1500
     browser.config.window_height = 1080
-    # закрытие сообщения о том что браузер запущен в отладочном режиме
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    # browser.config.driver_options = driver_options
 
     yield browser
 
